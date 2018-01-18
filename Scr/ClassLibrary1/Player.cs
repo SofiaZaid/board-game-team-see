225385281c9926c258bdf2571ff683e2ecd6d1df
Sofia Zaid <sofiazaid@gmail.com>
2018-01-18 15:43:07 +0100

    Added some classes to the game engine (class library with business logic for the MVC-application). Me and Emelie wrote some blueprints for methods in the Game.cs class in the GameEngine. These methods are to implemented, more methods also needed.

---
+++ Scr/ClassLibrary1/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Player
+    {
+        public string NickName { get; set; }
+        public int PlayerID { get; set; }  
+        
+        public int GamePieces { get; set; }
+    }
+}
