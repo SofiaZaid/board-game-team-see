07c6fc46f418818f010300115aea7fd527edc85f
emanu <emanu@LAPTOP-8OQNF1UG>
2018-01-19 14:29:33 +0100

    Spot version

--- Scr/ClassLibrary1/Class1.cs
+++ Scr/ClassLibrary1/Class1.cs
@@ -10,64 +10,8 @@ namespace ClassLibrary1
     public class GameMethod
     {
 
-        public bool ISWin()
-        {
-            return CheckForWinOnRows() && CheckForWinsOnDiagonal() && CheckForWinsOnColumns();
-
-        }
-
-        //Metod som sköter själva handlingen att placera ut en pjäs på brädet, i en speciell ruta.
-        public void PlaceGamePiece(int toX, int toY)
-        {
-            if (CheckIfFieldOnBoardIsFree(1, 1))
-            {
-
-            }
-
-        }
-
-        //Metod som kontrollerar om det är möjligt att lägga sin spelmarkör på ett specifikt fält på spelbrädet.
-        //Argumenten som metoden tar in är en x och en y - koordinat i rutnätet som utgör spelplanen.
-        public bool CheckIfFieldOnBoardIsFree(int x, int y)
-        {
-            return true;
-        }
-
-        //Method that controls if there are three similar game pieces/player marks in a row on the board. If So, return true.
-        public bool CheckForWinOnRows()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-
-            }
-            return true; 
-        }
-
-        //Method that controls if there are three similar game pieces/player marks on the diagonal on the board. If So, return true.
-        public bool CheckForWinsOnDiagonal()
-        {
-            return true;
-        }
-
-        //Method that controls if there are three similar game pieces/player marks in a column on the board. If So, return true.
-        public bool CheckForWinsOnColumns()
-        {
-            return true; 
-        }
-
-        //Method that controls if any of the "CheckForWinsOn..."-methods are evaluated to true. If so, the game has a winner and it is over.
-        public bool CheckIfGamehasAWinner()
-        {
-            return true; 
-        }
-
-        //Method that controls who the current player is, if current player is X then the value of currentPlayer is changed to O, and
-        //the other way around in the opposite case. Method is used to make it possible to switch turns in the game.
-        public void ChangePlayerTurn()
-        {
-
-
-        }
+        
+        
 
     }
 }
--- Scr/ClassLibrary1/Game.cs
+++ Scr/ClassLibrary1/Game.cs
@@ -9,9 +9,10 @@ namespace ClassLibrary1
     class Spot
     {
         public Player player;
-        public int place; 
+        public int x;
+        public int y;
     }
-   
+
     class Game
     {
         // Här skapar vi  av platser som ma kan placera en markör på. Listan innehåller 
@@ -22,12 +23,83 @@ namespace ClassLibrary1
         /// att uppdaters. T.ex. när en spelare placerar en markör på t.ex spot[4] så blir den rutan upptagen och rutan tillhör nu spelaren som klickade på den. 
         /// </summary>
         public static List<Spot> spot = new List<Spot>();
-        public Game(int xRow)
-        {
-            for (int i = 0; i < xRow * xRow; i++)
+        public Game()
+        {            
+
+            for (int i = 0; i < 3; i++)
             {
-                spot.Add(new Spot { player = null, place = i });
+                for (int x = 0; x< 3; x++)
+                {
+                    spot.Add(new Spot { player = null, x = i, y = x });
+                }
             }
+
+        }
+
+        //Metod som kontrollerar om det är möjligt att lägga sin spelmarkör på ett specifikt fält på spelbrädet.
+        //Argumenten som metoden tar in är en x och en y - koordinat i rutnätet som utgör spelplanen.
+        public bool CheckIfFieldOnBoardIsFree(List<Spot> x, int place)
+        {
+            if (x[place].player == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+            
+
+        //Metod som sköter själva handlingen att placera ut en pjäs på brädet, i en speciell ruta.
+
+        public void PlaceGamePiece(List<Spot> x, int place)
+        {
+
+            if (CheckIfFieldOnBoardIsFree(spot, place))
+            {
+                // placera markör
+            }
+            else
+            {
+                // Alert spot i taken
+            }
+
+        }
+
+       
+
+        //Method that controls if there are three similar game pieces/player marks in a row on the board. If So, return true.
+        public bool CheckForWinOnRows(List<Spot> spots)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+               
+            }
+            return false;
+        }
+
+        //Method that controls if there are three similar game pieces/player marks on the diagonal on the board. If So, return true.
+        public bool CheckForWinsOnDiagonal()
+        {
+            return true;
+        }
+
+        //Method that controls if there are three similar game pieces/player marks in a column on the board. If So, return true.
+        public bool CheckForWinsOnColumns()
+        {
+            return true;
+        }
+
+        //Method that controls if any of the "CheckForWinsOn..."-methods are evaluated to true. If so, the game has a winner and it is over.
+        public bool CheckIfGamehasAWinner()
+        {
+            return true;
+        }
+
+        //Method that controls who the current player is, if current player is X then the value of currentPlayer is changed to O, and
+        //the other way around in the opposite case. Method is used to make it possible to switch turns in the game.
+        public void ChangePlayerTurn()
+        {
+
 
         }
 
