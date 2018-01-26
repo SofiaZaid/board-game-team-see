7d8d8fd9728477a91c63bf25a0e8e47c9de00ce7
Sofia Zaid <sofiazaid@gmail.com>
2018-01-26 16:38:19 +0100

    Created test method to test PlaceMark and GetMarkAt methods in Game class

--- Scr/UnitTestProject1/UnitTest1.cs
+++ Scr/UnitTestProject1/UnitTest1.cs
@@ -31,5 +31,16 @@ namespace UnitTestProject1
             Assert.Equals(currentPlayer, Game.Mark.PlayerX);
         }
 
+        //Method that tests methods PlaceMark and GetMarkAt to see that after we have placed a mark on the gameboard 
+        //playerbefore is not the currentplayer and that the mark we now see placed on the board belongs to playerbefore.
+        public void CheckThatCorrectMarkIsReturnedDependingOnXAndYCoordinates()
+        {
+            Game game = new Game();
+            Game.Mark playerBefore = game.CurrentPlayer;
+            game.PlaceMark(2, 1);
+            Assert.AreNotEqual(playerBefore, game.CurrentPlayer);
+            Assert.AreEqual(playerBefore, game.GetMarkAt(2, 1));
+
+        }
     }
 }
