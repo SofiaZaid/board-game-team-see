51ecb19f1c3c32bd7479041a590318743ed4dd8a
Sofia Zaid <sofiazaid@gmail.com>
2018-01-26 16:15:59 +0100

    Made a test method for testing that the ChangePlayerTurn method works as expected.

--- Scr/UnitTestProject1/UnitTest1.cs
+++ Scr/UnitTestProject1/UnitTest1.cs
@@ -17,5 +17,19 @@ namespace UnitTestProject1
             Assert.AreEqual(3,result);
 
         }
+
+        //TestMethod that tests so that currentPlayer from the beginning is player X and so that the currentPlayer value is changed 
+        //into PlayerO when method ChangePlayerTurn is called.
+        [TestMethod]
+        public void CorrectlyChangingPlayerTurn()
+        {
+            Game game = new Game();
+            Game.Mark currentPlayer = game.CurrentPlayer;
+            game.ChangePlayerTurn();
+            Game.Mark newcurrentPlayer = game.CurrentPlayer;
+            Assert.Equals(newcurrentPlayer, Game.Mark.PlayerO);
+            Assert.Equals(currentPlayer, Game.Mark.PlayerX);
+        }
+
     }
 }
